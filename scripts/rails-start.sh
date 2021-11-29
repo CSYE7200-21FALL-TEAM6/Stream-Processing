6489293dce08d76b228ac8ac58609331058c50d4
ailec0623 <2523017180@qq.com>
2021-11-29 11:57:33 -0500

    start

--- scripts/rails-start.sh
+++ scripts/rails-start.sh
@@ -6,3 +6,4 @@ nohup /home/ubuntu/spark-3.2.0-bin-hadoop3.2-scala2.13/bin/spark-submit \
 --class edu.neu.csye7200.twitterproject.StreamProcessing \
 --master local \
 /home/ubuntu/spark/real_time_twitter_sentiment_analytics_system-1.0-SNAPSHOT-jar-with-dependencies.jar  > ~/application.log 2>&1 &
+echo -e "started" >> /home/ubuntu/started
