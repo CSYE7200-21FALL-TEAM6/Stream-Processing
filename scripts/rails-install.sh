23120545d14e23c36bd3b7b01734e908c01ec8ba
ailec0623 <2523017180@qq.com>
2021-11-28 21:26:52 -0500

    github action

--- scripts/rails-install.sh
+++ scripts/rails-install.sh
@@ -4,5 +4,5 @@ mkdir spark
 cp -r /home/ubuntu/spark-install/real_time_twitter_sentiment_analytics_system-1.0-SNAPSHOT-jar-with-dependencies.jar /home/ubuntu/spark/
 cd /home/ubuntu/spark-3.2.0-bin-hadoop3.2-scala2.13 || exit
 mkdir output
-cp -r /home/ubuntu/spark-install/output/ /home/ubuntu/spark-3.2.0-bin-hadoop3.2-scala2.13/output/
+cp -r /home/ubuntu/spark-install/output/ /home/ubuntu/spark-3.2.0-bin-hadoop3.2-scala2.13/
 cd /home/ubuntu/spark || exit
